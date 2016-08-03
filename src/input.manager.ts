c3dbc0ea07f345ad601a6ab02be59912abd3e8ca
Joaquim Rodolfo Marçal Galli <rodolfocop@gmail.com>
2016-08-03 19:14:55 -0400

    Adicionado os pacotes para passar o tslint

--- src/input.manager.ts
+++ src/input.manager.ts
@@ -1,106 +1,100 @@
-
-
 export class InputManager {
 
+  constructor(private input:HTMLInputElement, private options:any) {
+  }
 
-	constructor(private input: HTMLInputElement, private options: any) {
+  get rawValue() {
+    return this.input && this.input.value;
+  }
 
-	}
+  set rawValue(value) {
+    if (this.input) {
+      this.input.value = value;
+    }
+  }
 
-	get rawValue() {
-		return this.input && this.input.value;
-	}
+  get canInputMoreNumbers() {
 
-	set rawValue(value) {
-		if (this.input) {
-			this.input.value = value;
-		}
-	}
- 
-	get canInputMoreNumbers() {
+    let input = this.input;
+    let maxlength = input.maxLength;
 
-		let input = this.input;
-		let maxlength = input.maxLength;
+    var haventReachedMaxLength = !(this.rawValue.length >= maxlength && maxlength >= 0);
+    let selection = this.inputSelection;
+    let start = selection.start;
+    let end = selection.end;
+    let haveNumberSelected = (selection.start !== selection.end && input.value.substring(start, end).match(/\d/)) ? true : false;
+    let startWithZero = (input.value.substring(0, 1) === '0');
 
-		var haventReachedMaxLength = !(this.rawValue.length >= maxlength && maxlength >= 0);
-		let selection = this.inputSelection;
-		let start = selection.start;
-		let end = selection.end;
-		let haveNumberSelected = (selection.start !== selection.end && input.value.substring(start, end).match(/\d/)) ? true : false;
-		let startWithZero = (input.value.substring(0, 1) === "0");
+    return haventReachedMaxLength || haveNumberSelected || startWithZero;
+  }
 
-		return haventReachedMaxLength || haveNumberSelected || startWithZero;
-	}
-
-	get inputSelection() {
-		var el = this.input;
-		let start = 0;
-		let end = 0;
+  get inputSelection() {
+    var el = this.input;
+    let start = 0;
+    let end = 0;
 
 
-		if (typeof el.selectionStart === "number" && typeof el.selectionEnd === "number") {
-			start = el.selectionStart;
-			end = el.selectionEnd;
-		} else {
-			let range = (<any>document).selection.createRange(); // 
+    if (typeof el.selectionStart === 'number' && typeof el.selectionEnd === 'number') {
+      start = el.selectionStart;
+      end = el.selectionEnd;
+    } else {
+      let range = (<any>document).selection.createRange(); //
 
-			if (range && range.parentElement() === el) {
-				let len = el.value.length;
-				let normalizedValue = el.value.replace(/\r\n/g, "\n");
+      if (range && range.parentElement() === el) {
+        let len = el.value.length;
+        let normalizedValue = el.value.replace(/\r\n/g, '\n');
 
-				// Create a working TextRange that lives only in the input
-				let textInputRange = el.createTextRange();
-				textInputRange.moveToBookmark(range.getBookmark());
+        // Create a working TextRange that lives only in the input
+        let textInputRange = el.createTextRange();
+        textInputRange.moveToBookmark(range.getBookmark());
 
-				// Check if the start and end of the selection are at the very end
-				// of the input, since moveStart/moveEnd doesn't return what we want
-				// in those cases
-				let endRange = el.createTextRange();
-				endRange.collapse(false);
+        // Check if the start and end of the selection are at the very end
+        // of the input, since moveStart/moveEnd doesn't return what we want
+        // in those cases
+        let endRange = el.createTextRange();
+        endRange.collapse(false);
 
-				if (textInputRange.compareEndPoints("StartToEnd", endRange) > -1) {
-					start = end = len;
-				} else {
-					start = -textInputRange.moveStart("character", -len);
-					start += normalizedValue.slice(0, start).split("\n").length - 1;
+        if (textInputRange.compareEndPoints('StartToEnd', endRange) > -1) {
+          start = end = len;
+        } else {
+          start = -textInputRange.moveStart('character', -len);
+          start += normalizedValue.slice(0, start).split('\n').length - 1;
 
-					if (textInputRange.compareEndPoints("EndToEnd", endRange) > -1) {
-						end = len;
-					} else {
-						end = -textInputRange.moveEnd("character", -len);
-						end += normalizedValue.slice(0, end).split("\n").length - 1;
-					}
-				}
-			}
-		}
+          if (textInputRange.compareEndPoints('EndToEnd', endRange) > -1) {
+            end = len;
+          } else {
+            end = -textInputRange.moveEnd('character', -len);
+            end += normalizedValue.slice(0, end).split('\n').length - 1;
+          }
+        }
+      }
+    }
 
-		return {
-			start: start,
-			end: end
-		};
-	}
+    return {
+      start: start,
+      end: end
+    };
+  }
 
-	updateValueAndCursor(value, oldLen, startPos) {
-		let length = oldLen;
-		this.rawValue = value;
-		let newLength = value.length;
-		startPos = startPos - (length - newLength);
-		this.setCursorAt(startPos);
-	}
+  updateValueAndCursor(value, oldLen, startPos) {
+    let length = oldLen;
+    this.rawValue = value;
+    let newLength = value.length;
+    startPos = startPos - (length - newLength);
+    this.setCursorAt(startPos);
+  }
 
-	setCursorAt(pos) {
-		let elem = this.input;
-		if (elem.setSelectionRange) {
-			elem.focus();
-			elem.setSelectionRange(pos, pos);
-		} else if (elem.createTextRange) {
-			var range = elem.createTextRange();
-			range.collapse(true);
-			range.moveEnd("character", pos);
-			range.moveStart("character", pos);
-			range.select();
-		}
-	}
-
-
+  setCursorAt(pos) {
+    let elem = this.input;
+    if (elem.setSelectionRange) {
+      elem.focus();
+      elem.setSelectionRange(pos, pos);
+    } else if (elem.createTextRange) {
+      var range = elem.createTextRange();
+      range.collapse(true);
+      range.moveEnd('character', pos);
+      range.moveStart('character', pos);
+      range.select();
+    }
+  }
 }
--- src/money-input-event.handler.ts
+++ src/money-input-event.handler.ts
@@ -2,93 +2,90 @@ import {MoneyInputService} from './money-input.service';
 
 export class MoneyInputEventHandler {
 
-	inputService: MoneyInputService;
+  inputService:MoneyInputService;
 
-	constructor(private input: HTMLInputElement, options: any, onchange) {
-		this.inputService = new MoneyInputService(input, options, onchange);
-	}
+  constructor(private input:HTMLInputElement, options:any, onchange) {
+    this.inputService = new MoneyInputService(input, options, onchange);
+  }
 
-	handleKeypress(e) {
+  handleKeypress(e) {
+    const {inputService} = this;
+    let key = e.which || e.charCode || e.keyCode;
 
-		const {inputService} = this;
+    if (key === undefined) {
+      return false;
+    }
 
-		let key = e.which || e.charCode || e.keyCode;
+    if (key < 48 || key > 57) {
+      // -(minus) key
+      if (key === 45) {
+        inputService.changeSign();
+        return false;
+        // +(plus) key
+      } else if (key === 43) {
+        inputService.removeSign();
+        return false;
+        // enter key or tab key
+      } else if (key === 13 || key === 9) {
+        return true;
+      } else { // any other key with keycode less than 48 and greater than 57
+        e.preventDefault();
+        return true;
+      }
+    } else if (!inputService.canInputMoreNumbers) {
+      return false;
+    } else {
+      e.preventDefault();
+      inputService.addNumber(key);
+      return false;
+    }
 
-		if (key === undefined) {
-			return false;
-		}
+  }
 
-		if (key < 48 || key > 57) {
-			// -(minus) key
-			if (key === 45) {
-				inputService.changeSign()
-				return false;
-				// +(plus) key
-			} else if (key === 43) {
-				inputService.removeSign();
-				return false;
-				// enter key or tab key
-			} else if (key === 13 || key === 9) {
-				return true;
-			} else { // any other key with keycode less than 48 and greater than 57
-				e.preventDefault();
-				return true;
-			}
-		} else if (!inputService.canInputMoreNumbers) {
-			return false;
-		} else {
-			e.preventDefault();
+  handleKeydown(e) {
 
-			inputService.addNumber(key);
-			return false;
-		}
-
-	}
-
-	handleKeydown(e) {
-
-		const {inputService} = this;
+    const {inputService} = this;
 
 
-		let key = e.which || e.charCode || e.keyCode;
+    let key = e.which || e.charCode || e.keyCode;
 
-		if (key === undefined) {
-			return false;
-		}
+    if (key === undefined) {
+      return false;
+    }
+    /*
+      let selection = inputService.inputSelection;
+      let startPos = selection.start;
+      let endPos = selection.end;
+    */
 
-		let selection = inputService.inputSelection;
-		let startPos = selection.start;
-		let endPos = selection.end;
+    //espaço ou delete
+    if (key === 8 || key === 46 || key === 63272) {
+      e.preventDefault();
 
-		//espaço ou delete
-		if (key === 8 || key === 46 || key === 63272) {
-			e.preventDefault();
+      inputService.processSpacebar(key);
 
-			inputService.processSpacebar(key);
+      return false;
+    } else if (key === 9) { // tab
+      return true;
+    } else { // outros
+      return true;
+    }
+  }
 
-			return false;
-		} else if (key === 9) { // tab 
-			return true;
-		} else { // outros
-			return true;
-		}
+  handleBlur(e) {
+    this.inputService.reformatField();
+  }
 
-	}
+  handleClick(e) {
+    this.inputService.resetSelection();
+  }
 
-	handleBlur(e) {
-		this.inputService.reformatField();
-	}
+  handleFocus(e) {
+    this.inputService.saveFocusValue();
+  }
 
-	handleClick(e) {
-		this.inputService.resetSelection();
-	}
-
-	handleFocus(e) {
-		this.inputService.saveFocusValue();
-	}
-
-	handleCutPastEvent(e) {
-		this.inputService.waitAndFormat();
-	}
+  handleCutPastEvent(e) {
+    this.inputService.waitAndFormat();
+  }
 
 }
